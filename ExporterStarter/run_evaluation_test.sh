bc6744839691933ab897e28518f67a3d2c088b55
zz_y <zz_y@localhost>
2024-06-19 23:13:11 -0500

    change python3 to python

--- ExporterStarter/run_evaluation_test.sh
+++ ExporterStarter/run_evaluation_test.sh
@@ -1,4 +1,4 @@
 
-python3 ExportManager.py --config=num_samples_config.yml --targets=1 --timeseries=10 --windowsize=100 --querytype=sum --waiteval=1
+python ExportManager.py --config=num_samples_config.yml --targets=1 --timeseries=10 --windowsize=100 --querytype=sum --waiteval=1
 
 /bin/bash ./kill.sh
