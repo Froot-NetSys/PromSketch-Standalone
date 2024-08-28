fac24bb5444ab625a12214abb7faef756cbc0d75
zz_y <zz_y@localhost>
2024-08-27 22:06:31 -0400

    add branch

--- ExporterStarter/run_evaluation_10000ts_avg.sh
+++ ExporterStarter/run_evaluation_10000ts_avg.sh
@@ -1,2 +1,4 @@
 echo "--windowsize=10 --querytype=avg"
 python ExportManager.py --config=num_samples_config.yml --targets=200 --timeseries=10000 --max_windowsize=100000 --querytype=avg --waiteval=300
+
+./kill.sh
